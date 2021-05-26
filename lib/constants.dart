1a24efe122675b6c39f0003cc77042a3ed7abbc5
yashwant94308 <yashwant94308@gmail.com>
2021-05-26 18:02:40 +0530

    Initial steps for TheCatteringApp

---
+++ lib/constants.dart
@@ -0,0 +1,55 @@
+const FOOD_DATA = [
+  {
+    "name":"Aarti Sweets",
+    "brand":"Hawkers",
+    "price":4678349089,
+    "image":"burger.png"
+  },{
+    "name":"Cheese Dip",
+    "brand":"Hawkers",
+    "price":499,
+    "image":"cheese_dip.png"
+  },
+  {
+    "name":"Coke",
+    "brand":"Mcdonald",
+    "price":149,
+    "image":"cola.png"
+  },
+  {
+    "name":"Fries",
+    "brand":"Mcdonald",
+    "price":299,
+    "image":"fries.png"
+  },
+  {
+    "name":"Ice Cream",
+    "brand":"Ben & Jerry's",
+    "price":949,
+    "image":"ice_cream.png"
+  },
+  {
+    "name":"Noodles",
+    "brand":"Hawkers",
+    "price":449,
+    "image":"noodles.png"
+  },
+  {
+    "name":"Pizza",
+    "brand":"Dominos",
+    "price":1799,
+    "image":"pizza.png"
+  },
+  {
+    "name":"Sandwich",
+    "brand":"Hawkers",
+    "price":299,
+    "image":"sandwich.png"
+  },
+  {
+    "name":"Wrap",
+    "brand":"Subway",
+    "price":699,
+    "image":"wrap.png"
+  }
+];
---
+++ lib/home.dart
@@ -0,0 +1,286 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'constants.dart';
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final CategoriesScroller categoriesScroller = CategoriesScroller();   // made at line 198
+  ScrollController controller = ScrollController();
+  bool closeTopContainer = false;
+  double topContainer = 0;
+
+  List<Widget> itemsData = [];
+
+  void getPostsData() {
+    List<dynamic> responseList = FOOD_DATA;
+    List<Widget> listItems = [];
+    responseList.forEach((post) {
+      listItems.add(Container(
+          height: 150,
+          margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+          decoration: BoxDecoration(borderRadius: BorderRadius.all(Radius.circular(20.0)), color: Colors.white, boxShadow: [
+            BoxShadow(color: Colors.black.withAlpha(100), blurRadius: 10.0),
+          ]),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 20),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      post["name"],
+                      style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                    ),
+
+                    SizedBox(
+                      height: 10,
+                    ),
+
+                    Row(
+                      children:<Widget>[
+                        Icon(
+                          Icons.phone,
+                          color: Colors.blue[800],
+                        ),
+                        SizedBox(width:5.0),
+                        Text(
+                          " ${post["price"]}",
+                          style: const TextStyle(fontSize: 15, color: Colors.black, fontWeight: FontWeight.bold),
+
+                        ),
+                      ],
+                    )
+
+
+
+                  ],
+                ),
+                Image.asset(
+                  "assets/images/${post["image"]}",
+                  height: double.infinity,
+                )
+              ],
+            ),
+          )));
+    });
+    setState(() {
+      itemsData = listItems;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getPostsData(); // defined above line 35 - 40
+    controller.addListener(() {   // called above line 30-33
+
+      double value = controller.offset/150;
+
+      setState(() {
+        topContainer = value;
+        closeTopContainer = controller.offset > 50;   // called above line 30-33
+      });
+    });
+  }
+  Color primaryColor = Color(0xff18203d);
+  Color secondaryColor = Color(0xff232c51);
+  Color logoGreen = Color(0xff25bcbb);
+  @override
+  Widget build(BuildContext context) {
+    final Size size = MediaQuery.of(context).size;
+    final double categoryHeight = size.height*0.30;
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: primaryColor,
+        appBar: AppBar(
+
+          title: Text("Catering Service",
+            style: TextStyle(
+                color: Colors.black
+            ),
+          ),
+          centerTitle: false,
+          elevation: 0,
+          backgroundColor: Colors.cyan[100],
+          leading: IconButton(
+            icon: Icon(Icons.menu,
+              color: Colors.black,),
+            onPressed: () {
+            },
+          ),
+
+          actions: <Widget>[
+            IconButton(
+                icon: Icon(Icons.search, color: Colors.black),
+                onPressed: () {}
+            ),
+            IconButton(
+              icon: Icon(Icons.keyboard_voice, color: Colors.black),
+              onPressed: () {},
+            ),
+            IconButton(
+              icon: Icon(Icons.person, color: Colors.black),
+              onPressed: () {},
+            ),
+
+          ],
+        ),
+        body: Container(
+          // color: Colors.grey[400],
+          height: size.height,
+          child: Column(
+            children: <Widget>[
+              const SizedBox(
+                height: 10,
+              ),
+              AnimatedOpacity(
+                duration: const Duration(milliseconds: 150),
+                opacity: closeTopContainer?0:1,
+                child: AnimatedContainer(
+                    duration: const Duration(milliseconds: 150),
+                    width: size.width,
+                    alignment: Alignment.topCenter,
+                    height: closeTopContainer?0:categoryHeight,
+                    child: categoriesScroller),
+              ),
+              Expanded(
+                  child: ListView.builder(
+                      controller: controller,
+                      itemCount: itemsData.length,
+                      physics: BouncingScrollPhysics(),
+                      itemBuilder: (context, index) {
+                        double scale = 1.0;
+                        if (topContainer > 0.5) {
+                          scale = index + 0.5 - topContainer;
+                          if (scale < 0) {
+                            scale = 0;
+                          } else if (scale > 1) {
+                            scale = 1;
+                          }
+                        }
+                        return Opacity(
+                          opacity: scale,
+                          child: Transform(
+                            transform:  Matrix4.identity()..scale(scale,scale),
+                            alignment: Alignment.bottomCenter,
+                            child: Align(
+                                heightFactor: .95,
+                                alignment: Alignment.topCenter,
+                                child: itemsData[index]),
+                          ),
+                        );
+                      })),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class CategoriesScroller extends StatelessWidget {  // called at line 30 - 33
+  const CategoriesScroller();
+
+  @override
+  Widget build(BuildContext context) {
+    final double categoryHeight = MediaQuery.of(context).size.height * 0.30 - 50;
+    return SingleChildScrollView(
+      physics: BouncingScrollPhysics(),
+      scrollDirection: Axis.horizontal,
+      child: Container(
+        margin: const EdgeInsets.symmetric(vertical: 20, horizontal: 20),
+        child: FittedBox(
+          fit: BoxFit.fill,
+          alignment: Alignment.topCenter,
+          child: Row(
+            children: <Widget>[
+              Container(
+                width: 150,
+                margin: EdgeInsets.only(right: 20),
+                height: categoryHeight,
+                decoration: BoxDecoration(color: Colors.orange.shade400, borderRadius: BorderRadius.all(Radius.circular(20.0))),
+                child: Padding(
+                  padding: const EdgeInsets.all(12.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text(
+                        "Most\nFavorites",
+                        style: TextStyle(fontSize: 25, color: Colors.white, fontWeight: FontWeight.bold),
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Text(
+                        "20 Items",
+                        style: TextStyle(fontSize: 16, color: Colors.white),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Container(
+                width: 150,
+                margin: EdgeInsets.only(right: 20),
+                height: categoryHeight,
+                decoration: BoxDecoration(color: Colors.blue.shade400, borderRadius: BorderRadius.all(Radius.circular(20.0))),
+                child: Container(
+                  child: Padding(
+                    padding: const EdgeInsets.all(12.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                          "Trending",
+                          style: TextStyle(fontSize: 25, color: Colors.white, fontWeight: FontWeight.bold),
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Text(
+                          "20 Items",
+                          style: TextStyle(fontSize: 16, color: Colors.white),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              Container(
+                width: 150,
+                margin: EdgeInsets.only(right: 20),
+                height: categoryHeight,
+                decoration: BoxDecoration(color: Colors.lightBlueAccent.shade400, borderRadius: BorderRadius.all(Radius.circular(20.0))),
+                child: Padding(
+                  padding: const EdgeInsets.all(12.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text(
+                        "Super\nSaving",
+                        style: TextStyle(fontSize: 25, color: Colors.white, fontWeight: FontWeight.bold),
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Text(
+                        "20 Items",
+                        style: TextStyle(fontSize: 16, color: Colors.white),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
